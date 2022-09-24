ecc3c930fc01f1ddc2767f95214d75f2c6f9fe09
@Shamanovski <spike2@list.ru>
2022-09-24 15:25:58 +0600

    add block number counter, write balance and pubkey

--- extract_wallets/main.py
+++ extract_wallets/main.py
@@ -1,112 +1,38 @@
-import datetime
-import subprocess
 import json
-import os
-from tkinter import W
+import csv
+import re
 
-from wrapper import Reader, ReadAndWriteException
+import etherscan
+from eth_account import Account
 
-files = os.listdir("./cache")
-if "block-number.json" not in files:
-    with open("block-number.json", "w") as f:
-        json.dump(40000, f)
-if "unselected_walllets.json" not in files:
-    with open("block-unselected_walllets.json", "w") as f:
-        json.dump([], f)
-
-
-def call_proc(start_block, end_block):
-    try:
-        subprocess.run(
-            f"ethereumetl export_blocks_and_transactions -w 10 \
-                            --batch-size 5000 \
-                            --start-block {start_block} --end-block {end_block} \
-                            --transactions-output transactions.csv \
-                            --provider-uri https://mainnet.infura.io/v3/4f3a3997062a47bd98a80f3f965a8e89"                                                                                                                                                                                                                                                                                                                           ,
-            check=True,
-            shell=True,
-            capture_output=True,
-        )
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e.stderr}\n{e.stdout}")
-        print(f"Start block number: {start_block}")
-        with open("./cache/block-number.json", "w") as f:
-            json.dump(start_block, f)
-        raise e
-
-
-def get_wallet_and_hash(transactions_csv: Reader, wallets: dict):
-    for transaction in transactions_csv:
-        block_datetime = datetime.datetime.fromtimestamp(
-            int(transaction["block_timestamp"]))
-
-        account = transaction["from_address"]
-        tx_hash = transaction["transaction_hash"]
-        if account in wallets:
-            continue
-        
-        wallets[account] = tx_hash
-        
-        return block_datetime.year
-
+es = etherscan.Client(api_key='5NN9FSWUYFHSTBPIMCVYDXUYNS1M4RN2PQ',
+                      cache_expire_after=60)
+eth_account = Account()
 
 def main():
-    # transactions start showing up from block 40000
-    with open("./cache/block-number.json", "r") as f:
-        start_block = json.load(f)
+    with open('./cache/wallets.json') as f:
+        wallets = json.load(f)
 
-    with open("./cache/old_wallets.json") as file:
-        wallets : dict = json.load(file)
-        extract_old_wallets(start_block, wallets)
+    with open("result.csv", "a+") as f:
+        csv_writer = csv.DictWriter(f, fieldnames=["address", "balance", "pubkey"])
+        if f.tell() == 0:
+            csv_writer.writeheader()
 
-    try:
-        extract_valid_wallets(wallets, start_block)
-    finally:
-        print("Finished extracting wallets.")
-        with open("verified_wallets.json") as f:
-            json.dump(wallets, f)
+        write_balance_pubkey(wallets, csv_writer)
 
 
-
-def extract_old_wallets(start_block, wallets):
-    while True:
-        end_block = start_block + 50000
-        # read output from csv and write to unselected_wallets.csv
-        try:
-            call_proc(start_block, end_block)  # ?
-            transactions_csv = Reader("transactions.csv")
-            year = get_wallet_and_hash(transactions_csv, wallets)
-            if year == 2018:
-                return
-            print(f"Wallets with start block number {start_block} and "
-                  f"end block number {end_block} have been saved.")
-        except ReadAndWriteException as e:
-            print(f"Error: {e}")
-            print("Error while reading/writing to csv files.")
-            raise e
-        finally:
-            with open("wallets.json", "w") as f:
-                json.dump(wallets, f)
-
-
-
-
-def extract_valid_wallets(accounts, start_block):
-    while True:
-        end_block = start_block + 50000
-        call_proc(start_block, end_block)
-        transcations_csv = Reader("transactions.csv")
-        for transaction in transcations_csv:
-            block_datetime = datetime.datetime.fromtimestamp(
-                int(transaction["block_timestamp"]))
-            if block_datetime.year < 2018:
-                raise ReadAndWriteException(
-                    "Blocks datetime is less than 2018.")
-
-            account = transaction["from_address"]
-            if account in accounts:
-                print(f"{account} was used later than 2018: {block_datetime}")
-                accounts.pop(account)
+def write_balance_pubkey(accounts, csv_writer):
+    session = es.session
+    for account, tx_hash in accounts.items():
+        balance = es.get_eth_balance(account)
+        if balance < 1000:
+            print(f"Account: {account}, Balance: {balance} has more than 1000 ETH")
+            accounts.pop(account)
+            continue
+        response = session.get("https://etherscan.io/getRawTx?tx=" + tx_hash)
+        rawtx = re.search(r'Returned Raw Transaction Hex.*(0x.*?)\s', response.text).group(1)
+        pubkey = eth_account.recover_transaction(rawtx)
+        csv_writer.writerow({"address": account, "balance": balance, "pubkey": pubkey})
 
 
 main()
---
+++ extract_wallets/validate_wallets.py
@@ -0,0 +1,119 @@
+import datetime
+import enum
+import subprocess
+import json
+import os
+
+from yaml import BlockEndToken
+
+from wrapper import Reader, ReadAndWriteException
+
+files = os.listdir("./cache")
+if "block-number.json" not in files:
+    with open("./cache/block-number.json", "w") as f:
+        json.dump(40000, f)
+if "wallets.json" not in files:
+    with open("./cache/wallets.json", "w") as f:
+        json.dump({}, f)
+
+
+def call_proc():
+    try:
+        subprocess.run(
+            f"ethereumetl export_blocks_and_transactions -w 10 \
+                            --batch-size 5000 \
+                            --start-block {Block.start_block} --end-block {Block.start_block + 10000} \
+                            --transactions-output ./cache/transactions.csv \
+                            --provider-uri https://mainnet.infura.io/v3/4f3a3997062a47bd98a80f3f965a8e89"                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             ,
+            check=True,
+            shell=True,
+            capture_output=True,
+        )
+    except subprocess.CalledProcessError as e:
+        print(f"Error: {e.stderr}\n{e.stdout}")
+        print(f"Start block number: {Block.start_block}")
+        Block.cache_block_number()
+        raise e
+
+
+def add_wallet_and_hash(transactions_csv: Reader, old_wallets: dict):
+    for transaction in transactions_csv:
+        block_datetime = datetime.datetime.fromtimestamp(
+            int(transaction["block_timestamp"]))
+        if block_datetime.year == 2018:
+            return False
+
+        account = transaction["from_address"]
+        tx_hash = transaction["hash"]
+        if account in old_wallets:
+            continue
+
+        old_wallets[account] = tx_hash
+    
+    return True
+
+
+def main():
+    # transactions start showing up from block 40000
+    with open("./cache/block-number.json", "r") as f:
+        Block.start_block = json.load(f)
+
+    with open("./cache/wallets.json", "r") as f:
+        wallets: dict = json.load(f)
+    try:
+        extract_old_wallets(wallets)
+        print(f"Finished extracting old wallets. Number of wallets: {len(wallets)}")
+        extract_valid_wallets(wallets)
+    finally:
+        print(f"Finished extracting valid wallets. Number of wallets: {len(wallets)}")
+        Block.cache_block_number()
+        Block.cache_wallets(wallets)      
+
+    print("Wallets have been saved.")
+
+def extract_old_wallets(wallets):
+    while True:
+        call_proc()
+        transactions_csv = Reader("./cache/transactions.csv")
+        result = add_wallet_and_hash(transactions_csv, wallets)
+        if result == False:
+            return
+        print(f"Wallets with start block number {Block.start_block} and "
+            f"end block number {Block.start_block + 10000} have been saved.")
+        Block.start_block += 10000
+
+
+def extract_valid_wallets(wallets):
+    while True:
+        call_proc()
+        transcations_csv = Reader("./cache/transactions.csv")
+        for transaction in transcations_csv:
+            block_datetime = datetime.datetime.fromtimestamp(
+                int(transaction["block_timestamp"]))
+            if block_datetime.year < 2018:
+                print("Wallets with start block number {Block.start_block} has block datetime: {block_datetime}")
+                return
+
+            account = transaction["from_address"]
+            if account in wallets:
+                print(f"{account} was used later than 2018: {block_datetime}")
+                wallets.pop(account)
+        Block.start_block += 10000
+
+
+class Block(int):
+    start_block = 40000
+    
+
+    @classmethod
+    def cache_block_number(cls):
+        with open("./cache/block-number.json", "w") as f:
+            json.dump(cls.start_block, f)
+
+    @classmethod
+    def cache_wallets(cls, wallets):
+        with open("./cache/wallets.json", "w") as f:
+            json.dump(wallets, f)
+
+
+main()
